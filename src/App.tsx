ca77fc69d62857535fa76d8097a70222af146b4c
Koh Uehara <refluster@gmail.com>
2022-10-29 16:51:33 -0700

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import { Amplify, API, graphqlOperation } from 'aws-amplify'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//import awsExports from './aws-exports';
+//Amplify.configure(awsExports);
+
+import {   AmplifyProvider,
+  Authenticator,
+  Button,
+  Flex,
+  Image,
+  Text,
+  View,
+withAuthenticator, Heading } from '@aws-amplify/ui-react';
+
+import aws_exports from "./aws-exports";
+import "@aws-amplify/ui-react/styles.css";
+Amplify.configure(aws_exports);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyProvider> 
+      <Authenticator>
+        {({ signOut, user }) => (
+          <Flex
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+            alignContent="flex-start"
+            wrap="nowrap"
+            gap="1rem"
+            textAlign="center"
+          >
+            <View width="100%">
+              <Image src={logo} alt="logo" width={240} height={240} />
+            </View>
+
+            {user && (
+              <View width="100%">
+                <Text>Hello {user.username}</Text>
+                <Button onClick={signOut}>
+                  <Text>Sign Out</Text>
+                </Button>
+              </View>
+            )}
+
+            <View width="100%">
+              <Text>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </Text>
+            </View>
+          </Flex>
+        )}
+      </Authenticator>
+    </AmplifyProvider>
   );
-}
+};
 
 export default App;
