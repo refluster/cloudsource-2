20d21c2c8fe1a8e06283df01ad3d582f69b7bd81
Koh Uehara <refluster@gmail.com>
2022-11-01 07:24:12 -0700

    projects

---
+++ src/api/project.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { Project } from '../types/project.d';
+
+const baseUrl = 'https://h7r7m5r0oj.execute-api.us-west-2.amazonaws.com/dev';
+
+const getProjects = async (id?: string) => {
+    const url = `${baseUrl}/project`;
+    const config = {
+        headers: {
+        },
+    };
+    try {
+        const res = await axios.get<Project[]>(url, config);
+        console.log('data', res.data)
+        return res.data;
+    } catch(e) {
+        console.error("Error in getAllDevices():: ", e);
+    }
+};
+
+export default {
+    getProjects
+}
---
+++ src/types/project.d.ts
@@ -0,0 +1,8 @@
+export type Project = {
+    id: string;
+    name: string;
+    owner: string;
+    tokenTotal: number,
+    pointPerToken: number,
+    description: string,
+}
