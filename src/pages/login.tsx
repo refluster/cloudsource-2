c8ba461a7416eb0478ff0ff3929fa4675d90c3c1
Koh Uehara <refluster@gmail.com>
2022-11-01 23:32:03 -0700

    authenticator guard

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,20 +2,34 @@ import React, { useEffect, useState } from 'react'
 import { ThemeProvider } from '@emotion/react';
 import { customTheme } from '../styles/theme';
 
-import {   AmplifyProvider,
-  Authenticator,
+import {
+  AmplifyProvider,
+  Authenticator, useAuthenticator,
   Button,
   Flex,
   Image,
   Text,
   View,
-withAuthenticator, Heading } from '@aws-amplify/ui-react';
+  withAuthenticator, Heading
+} from '@aws-amplify/ui-react';
+import { useNavigate, useLocation } from 'react-router';
 
 import "@aws-amplify/ui-react/styles.css";
 
 export default function Login() {
+  const { route } = useAuthenticator((context) => [context.route]);
+  const location = useLocation();
+  const navigate = useNavigate();
+  let from = location.state?.from?.pathname || '/';
+  useEffect(() => {
+    console.log('effect, route', route);
+    if (route === 'authenticated') {
+      navigate(from, { replace: true });
+    }
+  }, [route, navigate, from]);
+
   return (
-    <AmplifyProvider> 
+    <AmplifyProvider>
       <Authenticator>
         {({ signOut, user }) => (
           <Flex
